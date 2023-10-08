5e30e2b1db45c981948ce06dd22e96566eb49b2b
r2 <jbrayden35@gmail.com>
2023-10-07 18:51:53 -0700

    Step 2 complete: added counter to button

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName =
-  "If you press a broken button enough times does it do something??";
+  "How many boops til the great ascii frog offers wisdom?";
 
 document.title = gameName;
 
@@ -11,10 +11,29 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-// Create the button element
+// make button
 const button = document.createElement("button");
 button.className = "favorite styled ascii-art";
 button.type = "button";
 
+// counter
+let counter = 0;
+
+// div to display the counter
+const counterText = document.createElement("div");
+counterText.id = "counterText";
+
+// Function to update and display the counter
+function updateCounter() {
+  counter++;
+  counterText.textContent = `${counter} boops`;
+}
+
+// click event for button
+button.addEventListener("click", updateCounter);
+
+// Append the button with counter
+app.append(button, counterText);
+
 // Append the button element
-app.append(button);
+//app.append(button);
