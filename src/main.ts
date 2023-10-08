ac9afb247a29dd4c61ea198d6158e1d09283ce44
r2 <jbrayden35@gmail.com>
2023-10-07 19:20:13 -0700

    Step 4 complete: added frame decimal counter

--- src/main.ts
+++ src/main.ts
@@ -34,11 +34,47 @@ button.addEventListener("click", updateCounter);
 // Append the button with counter
 app.append(button, counterText);
 
-// Function to increment count every second
-function increment_every_second() {
-    counter++;
-    counterText.textContent = `${counter} boops`;
+////////////////////////////////////////////////////
+// INCREMENT BY SECONDS
+////////////////////////////////////////////////////
+// // Function to increment count every second
+// function increment_every_second() {
+//   counter++;
+//   counterText.textContent = `${counter} boops`;
+// }
+
+// // setInterval to call increment_every_second function
+// setInterval(increment_every_second, 1000);
+////////////////////////////////////////////////////
+
+// last for requestAnimationFrame
+let lastTimestamp = 0; // Timestamp of the last frame
+
+// Function to update the counter based on time elapsed from the last frame
+function update_decimal_counter(timestamp: number) {
+  if (!lastTimestamp) {
+    lastTimestamp = timestamp;
+  }
+
+  // conversion to seconds
+  const secondsElapsed = (timestamp - lastTimestamp) / 1000;
+  // 60 frames per second
+  const decIncrease = 1 / 60;
+
+  // Calculate the increment
+  const increment = secondsElapsed * decIncrease;
+
+  // increment counter
+  counter += increment;
+
+  // Display the counter two decimal places
+  counterText.textContent = `${counter.toFixed(2)} boops`;
+
+  lastTimestamp = timestamp;
+
+  // Request the next animation frame
+  requestAnimationFrame(update_decimal_counter);
 }
-  
-// setInterval to call increment_every_second function
-setInterval(increment_every_second, 1000);
+
+// Start the animation loop
+requestAnimationFrame(update_decimal_counter);
