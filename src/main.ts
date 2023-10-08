475b0fc86c77600ae80ff00163dd4250d6956f18
r2 <jbrayden35@gmail.com>
2023-10-07 17:30:58 -0700

    changed name of game

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName =
-  "If you press a broken button enough times does it do something?";
+  "If you press a broken button enough times does it do something??";
 
 document.title = gameName;
 
